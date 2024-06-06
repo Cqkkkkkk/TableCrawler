c9b353e251abe3ffcc55c77fe1e26497b8a38a3f
cq <chenqink@outlook.com>
2024-06-06 23:24:39 +0800

    fix scroll bug. add data

--- main_selenium.py
+++ main_selenium.py
@@ -26,9 +26,9 @@ def get_os_specific_download_path():
 def selenium_fetch_download(conn, source_url, destination_dir):
     # Setup Chrome options
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Ensure GUI is off
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--headless")  # Ensure GUI is off
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
 
     # prefs = {
     #     "download.default_directory": './temp_download',  # Set download directory
@@ -39,37 +39,39 @@ def selenium_fetch_download(conn, source_url, destination_dir):
     # chrome_options.add_experimental_option("prefs", prefs)
 
     # Set path to chromedriver as per your configuration
-    webdriver_service = Service("/opt/homebrew/bin/chromedriver") # Change this to your own chromedriver path
-
+    # webdriver_service = Service("/opt/homebrew/bin/chromedriver") # Change this to your own chromedriver path
+    webdriver_service = Service() 
     # Choose Chrome Browser
     driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
     # Open the website
     driver.get(source_url)
+    # pdb.set_trace()
 
     # Wait for the page to load (adjust the time as necessary)
     time.sleep(3) # You can use WebDriverWait for a more efficient wait
 
     # Scroll down to load more content until no more new content loads
-    # scroll_pause_time = 2
-    # last_height = driver.execute_script("return document.body.scrollHeight")
-    # print(f"Height: {last_height}")
+    scroll_pause_time = 3
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    print(f"Height: {last_height}")
 
-    # while True:
-    #     # Scroll down to the bottom
-    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    #     # driver.execute_script("window.scrollTo(0, 999999)")
+    while True:
+        # Scroll down to the bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    #     # Wait to load the page
-    #     time.sleep(scroll_pause_time)
+        # Wait to load the page
+        time.sleep(scroll_pause_time)
 
-    #     # Check if new content is loaded
-    #     new_height = driver.execute_script("return document.body.scrollHeight")
-    #     if new_height == last_height:
-    #         break
-    #     last_height = new_height
+        # Check if new content is loaded
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
 
-    #     print(f"New height: {new_height}")
+        print(f"New height: {new_height}")
+        if new_height >= 50000:
+            break
 
     # Find all elements that match the given selector and extract their href attributes
     elements = driver.find_elements(By.CSS_SELECTOR, 'a[data-testid="template-thumbnail-card-renderer-a"]')
@@ -78,7 +80,7 @@ def selenium_fetch_download(conn, source_url, destination_dir):
     urls = [element.get_attribute('href') for element in elements]
     for url in urls:
         insert_table_info(conn, 'undownloaded', url)
-
+    
     # Iterate through each href and click the download button
     cur_url = fetch_undownloaded_url(conn)
     while cur_url:
@@ -104,7 +106,7 @@ def selenium_fetch_download(conn, source_url, destination_dir):
                 update_table_name(conn, cur_url, downloaded_file_name)
 
         except Exception as e:
-            print(f"Failed to download from {cur_url}: {e}")
+            pass
 
         cur_url = fetch_undownloaded_url(conn)
 
@@ -117,9 +119,9 @@ if __name__ == "__main__":
     create_database_and_table(conn)
 
     source_url = """
-    https://create.microsoft.com/en-us/search?query=Employee%20recognition&filters=excel
+    https://create.microsoft.com/en-us/search?query=invoices&filters=excel
     """
-    name = "employee-recognition".replace(" ", "_").lower()
+    name = "invoce".replace(" ", "_").lower()
     destination_dir = f"./storage/{name}"
     selenium_fetch_download(conn, source_url, destination_dir)
 
